f9695f30e60cd63e8cfcdc60a6f0eea531e759de
Sisu <siongesslbauer1402@gmail.com>
2026-06-04 00:17:58 +0200

    feat: added movemt for queen

--- core/Schach.Core/Models/Queen.cs
+++ core/Schach.Core/Models/Queen.cs
@@ -27,6 +27,31 @@ public class Queen : Figure
 
    public override List<(int row, int col)> GetAvailableMoves(Board board)
    {
-      throw new NotImplementedException();
+      var moves = new List<(int row, int col)>();
+
+      (int rowOffset, int colOffset)[] offsets = { (1, 1), (1, -1), (1, 0), (-1, 1), (-1, -1), (-1, 0), (0, -1), (0, 1) };
+
+      foreach ((int rowOffset, int colOffset) in offsets)
+      {
+         int newRow = _position.row + rowOffset;
+         int newCol = _position.col + colOffset;
+
+         while (!(newRow > 7 || newRow < 0 || newCol > 7 || newCol < 0))
+         {
+            Figure figureOnSquare = board.Figures.FirstOrDefault(t => t.Position == (newRow, newCol));
+
+            if (figureOnSquare != null && figureOnSquare.Color == _color)
+            {
+               continue;
+            }
+
+            moves.Add((newRow, newCol));
+
+            newRow += rowOffset;
+            newCol += colOffset;
+         }
+      }
+
+      return moves;
    }
 }
