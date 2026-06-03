0efc5b0145c1f012a61c016979b11da5009d5805
Sisu <siongesslbauer1402@gmail.com>
2026-06-04 00:19:18 +0200

    feat: added movement for rook

--- core/Schach.Core/Models/Rook.cs
+++ core/Schach.Core/Models/Rook.cs
@@ -12,12 +12,12 @@ public class Rook : Figure
 
    public override void Move()
    {
-      
+
    }
 
    public override void Take()
    {
-      
+
    }
 
    public override void OnClick_ShowAvailableMoves()
@@ -27,6 +27,31 @@ public class Rook : Figure
 
    public override List<(int row, int col)> GetAvailableMoves(Board board)
    {
-      throw new NotImplementedException();
+      var moves = new List<(int row, int col)>();
+
+      (int rowOffset, int colOffset)[] offsets = { (0, 1), (0, -1), (-1, 0), (-1, 0) };
+
+      foreach ((int rowOffset, int colOffset) in offsets)
+      {
+         int newRow = _position.row + rowOffset;
+         int newCol = _position.col + colOffset;
+
+         while (!(newRow > 7 || newRow < 0 || newCol > 7 || newCol < 0))
+         {
+            Figure figureOnSquare = board.Figures.FirstOrDefault(t => t.Position == (newRow, newCol));
+
+            if (figureOnSquare != null && figureOnSquare.Color == _color)
+            {
+               continue;
+            }
+
+            moves.Add((newRow, newCol));
+
+            newRow += rowOffset;
+            newCol += colOffset;
+         }
+      }
+
+      return moves;
    }
 }
